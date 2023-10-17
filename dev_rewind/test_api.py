cf1363ddbfa657b2321ba6ad97c02402ac3d8c60
williamfzc <178894043@qq.com>
2023-10-17 21:27:12 +0800

    chore: case for embed

--- dev_rewind/test_api.py
+++ dev_rewind/test_api.py
@@ -1,10 +1,28 @@
+import pytest
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.vectorstores.chroma import Chroma
+
 from dev_rewind import DevRewind, DevRewindConfig
 
 
-def test_document():
+@pytest.fixture
+def doc_fixture():
     config = DevRewindConfig()
     config.repo_root = ".."
     api = DevRewind(config)
-    docs = api.collect_documents()
-    assert docs
+    return api.collect_documents()
 
+
+def test_document(doc_fixture):
+    assert doc_fixture
+
+
+def test_embed(doc_fixture):
+    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    db = Chroma.from_documents(doc_fixture, embeddings)
+    print("There are", db._collection.count(), "in the collection")
+
+    query = "init"
+    matching_docs = db.similarity_search(query)
+    result = matching_docs[0]
+    print(result)

e03344c65baca4656fd3194a5be8bc941c134e1a
williamfzc <178894043@qq.com>
2023-10-19 19:46:23 +0800

    fix: case path

--- dev_rewind/test_core.py
+++ dev_rewind/test_core.py
@@ -10,7 +10,7 @@ from dev_rewind import DevRewind, DevRewindConfig
 @pytest.fixture
 def meta_fixture():
     config = DevRewindConfig()
-    config.repo_root = ".."
+    config.repo_root = os.path.dirname(__file__)
     api = DevRewind(config)
     return api.collect_metadata()
 
@@ -36,7 +36,7 @@ def test_embed(meta_fixture):
 
 def test_with_openai(meta_fixture):
     if "OPENAI_API_KEY" not in os.environ:
-        pytest.skip()
+        pytest.skip("no openapi key for test")
 
     api = DevRewind()
     retriever = api.create_retriever(ctx=meta_fixture)
